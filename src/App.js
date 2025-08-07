f7ea7fd0758cbe45e423a51d0377779e46993506
FellanZ <zimmerfellan@gmail.com>
2025-08-07 20:14:42 +0200

    Added the GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/FellanZ" target="_blank">
+          Felicia Z
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/FellanZ/weather-search" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
